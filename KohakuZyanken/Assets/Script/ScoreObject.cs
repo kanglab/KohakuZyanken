77b9986c193a594d61ac2f825809c5e815692ef1
SuzukiTomoyuki <dxfsrs1004@gmail.com>
2017-06-09 16:57:04 +0900

    clean up

---
+++ KohakuZyanken/Assets/Script/ScoreObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreObject : MonoBehaviour {
+
+	public GameObject summaryText;
+	public GameObject winRateText;
+
+	// Use this for initialization
+	void Start () {
+		initScoreText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void initScoreText(){
+		summaryText.SetActive(false);
+		winRateText.SetActive(false);
+	}
+}
